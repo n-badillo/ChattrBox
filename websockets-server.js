0e3d0ceae5f9b838df836382aafbb35e9c6092e6
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-08 20:39:15 -0700

    16.  Create the Chat Server Functionality

--- websockets-server.js
+++ websockets-server.js
@@ -5,14 +5,23 @@ var ws = new WebSocketServer({
   port: port
 });
 
+var messages = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function(socket){
   console.log('client connection established');
 
+  messages.forEach(function (msg){
+    socket.send(msg);
+  });
+
   socket.on('message', function(data){
     console.log('message recieved: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach(function (clientSocket){
+      clientSocket.send(data);
+    });
   });
 
 });
