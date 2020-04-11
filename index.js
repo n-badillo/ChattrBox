f899d792b75179012c7ab6f9adc51b4c56c11145
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-11 13:34:16 -0700

    15.  Add Bronze and Silver Challenges

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var http = require('http');    // Using node.js' built-in function to access the
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
+var errorPage = require('./error_module');
+var mime = require('mime');  // CH 15.  Silver Challenge
 
 
 var handleError = function (err,res) {
   res.writeHead(404);
   res.end();
+  
 };
 
+
+var showErrorPage = function(err, res) {
+  fs.readFile('app/error.html', function(err,data){
+    res.setHeader('content-type', 'text/html');
+    res.end(data);
+  });
+};
 
 var server = http.createServer(function (req, res){
   // http.createServe takes in one argument, a function. This function is called for every HTTP request.
@@ -20,10 +30,13 @@ var server = http.createServer(function (req, res){
 
   fs.readFile(filePath, function(err, data){
     if(err) {
-      handleError(err, res);
+      //handleError(err, res);
+      showErrorPage(err, res);
       return;
     } else {
-    res.end(data); }
+      var mimeType = mime.getType(filePath);  // Gets the content type of the filePath
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data); }
   });
 });
 
--- websockets-server.js
+++ websockets-server.js
@@ -1,28 +1,27 @@
-var WebSocket = require('ws');
+var WebSocket = require("ws");
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
 var ws = new WebSocketServer({
-  port: port
+  port: port,
 });
 
 var messages = [];
 
-console.log('websockets server started');
+console.log("websockets server started");
 
-ws.on('connection', function(socket){
-  console.log('client connection established');
+ws.on("connection", function (socket) {
+  console.log("client connection established");
 
-  messages.forEach(function (msg){
+  messages.forEach(function (msg) {
     socket.send(msg);
   });
 
-  socket.on('message', function(data){
-    console.log('message recieved: ' + data);
+  socket.on("message", function (data) {
+    console.log("message recieved: " + data);
     messages.push(data);
-    ws.clients.forEach(function (clientSocket){
+    ws.clients.forEach(function (clientSocket) {
       clientSocket.send(data);
+      clientSocket.send(data); // Bronze challenge: Am I repeating myself?
     });
   });
-
 });
-
