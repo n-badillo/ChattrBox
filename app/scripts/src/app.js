ae9734c35a67f3ec66cc5930d5a8894836e6d359
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-04-15 21:58:44 -0700

    18.  Create the ChatForm Class

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,11 +1,19 @@
 import socket from './ws-client';
+import { ChatForm } from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat ="message-input"]';
 
 class ChatApp{
   constructor(){
+    this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({ message: 'pow!' });
-      socket.sendMessage(message.serialize());
+      this.chatForm.init((data) => {
+        let message = new ChatMessage(data);
+        socket.sendMessage(message.serialize());
+      });
     });
     socket.registerMessageHandler((data) => {
       console.log(data);
